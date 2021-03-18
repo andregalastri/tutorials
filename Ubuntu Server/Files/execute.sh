6aec491764aa0608a5b1aac9d2c9a19eebc7e76a
André Galastri <49572917+andregalastri@users.noreply.github.com>
2021-03-18 17:35:54 -0300

    Update execute.sh

--- Ubuntu Server/Files/execute.sh
+++ Ubuntu Server/Files/execute.sh
@@ -54,6 +54,15 @@ sudo a2enmod mpm_event proxy_fcgi setenvif
 sudo service apache2 restart
 
 clear
+echo "INSTALLING COMPOSER"
+sleep 5
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo rm composer-setup.php
+
+clear
 echo "DONE!"
 echo "- Now, configure PHPMyAdmin virtual host in: "
 echo ""
