590eec9eb4dfdc3173017edf292c91f699cb3c30
andregalastri <contato@andregalastri.com.br>
2022-08-23 14:15:55 +0000

    Update

--- Arch/base.sh
+++ Arch/base.sh
@@ -17,7 +17,7 @@ echo "|||||||||||||||||  DONE  |||||||||||||||||||"
 sleep 3
 echo "Installing applications"
 sleep 3
-echo "S" | sudo pacman -S xorg-server sddm openbox xfce4-terminal ranger thunar gvfs mousepad nano virtualbox-guest-iso virtualbox-guest-utils nitrogen polybar rofi git go
+(echo "2";echo "S") | sudo pacman -S xorg-server sddm openbox xfce4-terminal ranger thunar gvfs mousepad nano virtualbox-guest-iso virtualbox-guest-utils nitrogen polybar rofi git go
 
 echo "|||||||||||||||||  DONE  |||||||||||||||||||"
 sleep 3
@@ -26,6 +26,9 @@ sleep 3
 echo "S" | sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
-echo "S" | makepkg
-echo "S" | makepkg -si
+(echo "S";echo "S") | makepkg -si
 cd ~
+
+sudo systemctl enable sddm.service
+mkdir -p ~/.config/openbox/
+cp -r /etc/xdg/openbox/* ~/.config/openbox/
