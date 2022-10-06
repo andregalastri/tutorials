93ee5bd1560c667b54cf3d12405e449d9ef991c7
andregalastri <contato@andregalastri.com.br>
2022-10-06 00:04:08 +0000

    test

--- Arch/install.sh
+++ Arch/install.sh
@@ -304,33 +304,41 @@ Installs more applications from AUR.";
 
 # RAR
 # Allows you to extract and compress RAR files. It automatically integrates with Xarchive.
-packages=("rar");
+# packages+=("rar");
 
 # GOOGLE CHROME
 # Say what you want, but I like Google Chrome because it is compatible with everything and has the best Adblocks around the internet. As a user, I like that some things just work. I also tried some minimal browsers. The only one I really enjoyed was Qutebrowser, but it lacks a functional Adblock.
-packages+=("google-chrome");
+# packages+=("google-chrome");
 
 # VISUAL STUDIO CODE
 # Code editor that I use for programming.
-#packages+=("visual-studio-code-bin");
+# packages+=("visual-studio-code-bin");
 
 # TELEGRAM
 # Desktop version of Telegram messenger
-#packages+=("telegram-desktop");
+# packages+=("telegram-desktop-bin");
 
 # WARSAW
 # Needed if you access Internet Banking websites
-#packages+=("warsaw-bin");
+# packages+=("warsaw-bin");
 
 # FREE FILE SYNC
 # Application that I use to syncronize my files with my external hard drive. Good to create backups.
-#packages+=("freefilesync-bin");
+# packages+=("freefilesync-bin");
+
+# PARCELLITE CLIPBOARD MANAGER
+# Without a clipboard manager, you copy/paste isn't persistent.
+packages+=("parcellite");
+
+# DMENU FOR NETWORK MANAGER
+# Allows launch Network Manager with Dmenu
+packages+=("networkmanager-dmenu-git");
 
 # FONTS
 # More fonts from AUR.
-packages+=("ttf-roboto-mono ttf-roboto ttf-century-gothic");
+packages+=("ttf-roboto-mono ttf-roboto ttf-century-gothic nerd-fonts-noto");
 
-(echo "y") | LANG=C yay --noprovides --answerdiff None --answerclean All --mflags --noconfirm --removemake --needed -S ${packages[*]};
+(echo "y") | LANG=C yay --answerdiff None --answerclean All --noconfirm --removemake --needed -S ${packages[*]};
 fc-cache -f -v;
 
 Done;
